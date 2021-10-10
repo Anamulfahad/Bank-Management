8c632dca1ab82083c332e17d6b98cd6e5ac2d789
Anamulfahad <ahasan192110@bscse.uiu.ac.bd>
2021-10-10 11:29:37 +0600

    added userView(); fetchInfo(); connectToServer();

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -11,9 +11,12 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
 
 public class LoginController {
+    BufferedReader reader;
+    BufferedWriter writer;
 
     @FXML
     private TextField phoneNumber;
@@ -25,7 +28,7 @@ public class LoginController {
     private Label comment;
 
     @FXML
-    void logIn() throws IOException, ClassNotFoundException {
+    private void logIn(ActionEvent event) throws IOException, ClassNotFoundException {
         String s1 = phoneNumber.getText();
         String s2 = password.getText();
 
@@ -35,15 +38,50 @@ public class LoginController {
         if (loginSupport.accountExist(s1, s2)) {
             System.out.println("login successful");
             comment.setText("valid");
+
+            fetchInfo(s1);
+
+            userView(event);
         } else comment.setText("invalid");
     }
 
     @FXML
-    void newAccount(ActionEvent event) throws IOException {
+    private void newAccount(ActionEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("SignUpForm.fxml"));
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(new Scene(parent));
         window.setResizable(false);
         window.show();
+    }
+
+    private void userView(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("UserView.fxml"));
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(new Scene(parent));
+        window.setResizable(false);
+        window.show();
+    }
+
+    private void fetchInfo(String phone) throws IOException {
+        connectToServer();
+
+        writer.write("fetchInfo\n");
+        writer.flush();
+
+        writer.write(phone + "\n");
+        writer.flush();
+
+
+
+    }
+
+    private void connectToServer() throws IOException {
+        Socket socket = new Socket("localhost", 1234);
+
+        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+        reader = new BufferedReader(inputStreamReader);
+
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+        writer = new BufferedWriter(outputStreamWriter);
     }
 }
